22ae6c4581442a0ce837773afad6bf5468bb3252
Miles Higbie <miles.higbie@gmail.com>
2020-02-04 17:41:12 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,5 +3,30 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+  
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.new(student_params)
+      if @student.save 
+        redirect_to student_path(@student)
+      else
+        flash.now[:message] = @student.errors.full_messages[0]
+        render :new
+      end
+  end 
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
+  end
+
+  
+  
 
 end
